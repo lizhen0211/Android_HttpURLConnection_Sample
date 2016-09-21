ab5452c4f7a8f861f234e388a2eaef6eeb181f02
lizhen <lizhen@zhumengyuan.com>
2016-09-21 10:25:03 +0800

    增加post urlencode请求

--- app/src/main/java/com/example/lz/android_httpurlconnection_sample/MainActivity.java
+++ app/src/main/java/com/example/lz/android_httpurlconnection_sample/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 import com.example.lz.android_httpurlconnection_sample.net.HttpRequest;
 import com.example.lz.android_httpurlconnection_sample.net.HttpResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView resultTextView;
@@ -41,8 +44,48 @@ public class MainActivity extends AppCompatActivity {
         request.start();
     }
 
-    public void onPostPairClick(View view) {
+    public void onPostURLEncodedClick(View view) {
+        // FIXME: 2016/9/21 lz 替换自己的服务器域名
+        String url = "http://www.baidu.com";
+        HttpRequest request = new HttpRequest(url, HttpRequest.MethodType.POST, new HttpResponse.Listener() {
+            @Override
+            public void onResponse(final HttpResponse result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultTextView.setText(result.responseContents);
+                    }
+                });
+            }
+        }, new HttpResponse.ErrorListener() {
+            @Override
+            public void onErrorResponse(final int errorCode, final String errorResult) {
+                System.out.print(errorCode);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultTextView.setText(errorCode + ":" + errorResult);
+                    }
+                });
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                // FIXME: 2016/9/21 增加你的请求参数
+                params.put("testKey", "testValue");
+                return params;
+            }
 
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Connection", "keep-alive");
+                // FIXME: 2016/9/21 添加你的头文件信息
+                return headers;
+            }
+        };
+        request.start();
     }
 
     public void onPostJsonClick(View view) {
--- app/src/main/java/com/example/lz/android_httpurlconnection_sample/net/HttpRequest.java
+++ app/src/main/java/com/example/lz/android_httpurlconnection_sample/net/HttpRequest.java
@@ -6,9 +6,13 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * Created by lz on 2016/9/20.
@@ -16,6 +20,11 @@ import java.net.URL;
 public class HttpRequest {
 
     private static final String TAG = "HttpRequest";
+
+    private static final String DEFAULT_PARAMS_ENCODING = "UTF-8";
+
+    private final static String HEADER_CONTENT_TYPE = "Content-Type";
+
     /**
      * 请求url
      */
@@ -67,10 +76,17 @@ public class HttpRequest {
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod(methodType.value);
                 conn.setDoInput(true);
-                //conn.setDoOutput(true);
-                //conn.setRequestProperty("Connection", "Keep-Alive");
-                //conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+                conn.setDoOutput(true);
                 conn.setUseCaches(false);
+                if (methodType == MethodType.POST) {
+                    addHeader(conn);
+                    byte[] body = getBody();
+                    if (body != null) {
+                        conn.getOutputStream().write(body);
+                    }
+                } else if (methodType == MethodType.GET) {
+                    //do something
+                }
                 //在对各种参数配置完成后，通过调用connect方法建立TCP连接，但是并未真正获取数据
                 //conn.connect()方法不必显式调用，当调用conn.getInputStream()方法时内部也会自动调用connect方法
                 conn.connect();
@@ -97,6 +113,56 @@ public class HttpRequest {
                     conn.disconnect();
                 }
             }
+        }
+    }
+
+    public String getBodyContentType() {
+        return "application/x-www-form-urlencoded; charset=" + getParamsEncoding();
+    }
+
+    protected String getParamsEncoding() {
+        return DEFAULT_PARAMS_ENCODING;
+    }
+
+    public Map<String, String> getHeaders() {
+        return Collections.emptyMap();
+    }
+
+
+    private void addHeader(HttpURLConnection conn) {
+        conn.setRequestProperty(HEADER_CONTENT_TYPE, getBodyContentType());
+        Map<String, String> headers = getHeaders();
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                conn.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    public byte[] getBody() {
+        Map<String, String> params = getParams();
+        if (params != null && params.size() > 0) {
+            return encodeParameters(params, getParamsEncoding());
+        }
+        return null;
+    }
+
+    protected Map<String, String> getParams() {
+        return null;
+    }
+
+    private byte[] encodeParameters(Map<String, String> params, String paramsEncoding) {
+        StringBuilder encodedParams = new StringBuilder();
+        try {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                encodedParams.append(URLEncoder.encode(entry.getKey(), paramsEncoding));
+                encodedParams.append('=');
+                encodedParams.append(URLEncoder.encode(entry.getValue(), paramsEncoding));
+                encodedParams.append('&');
+            }
+            return encodedParams.toString().getBytes(paramsEncoding);
+        } catch (UnsupportedEncodingException uee) {
+            throw new RuntimeException("Encoding not supported: " + paramsEncoding, uee);
         }
     }
 
